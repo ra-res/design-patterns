e6ca8b7facc259e3a48627d1d4acb76352fbc8e9
ra-res <ra-res@users.noreply.github.com>
2021-03-25 17:01:51 +0000

    composite

--- structuralPatterns/Composite.ts
+++ structuralPatterns/Composite.ts
@@ -5,6 +5,8 @@
  * @format
  */
 
+import { component } from "vue/types/umd";
+
 abstract class Component {
   protected parent: Component;
 
@@ -68,7 +70,76 @@ class Leaf extends Component {
  */
 class Composite extends Component {
   protected children: Component[] = [];
+
+  public add(component: Component): void {
+    this.children.push(component);
+    component.setParent(this);
+  }
+
+  public remove(component: Component): void {
+    const componentIndex = this.children.indexOf(component);
+    this.children.splice(componentIndex, 1);
+    component.setParent(null);
+  }
+
+  public isComposite(): boolean {
+    return true;
+  }
+  /**
+   * The Composite executes its primary logic in a particular way. It
+   * traverses recursively through all its children, collecting and summing
+   * their results. Since the composite's children pass these calls to their
+   * children and so forth, the whole object tree is traversed as a result.
+   */
   public operation(): string {
-    throw new Error("Method not implemented.");
+    const result = [];
+    for (const child of this.children) {
+      result.push(child.operation());
+    }
+    return `Branch(${result.join("+")})`;
   }
 }
+
+/**
+ * The client code works with all of the components via the base interface.
+ */
+const compositeClientCode = (component: Component) => {
+  console.log(`Result: ${component.operation()}`);
+};
+
+const simple = new Leaf();
+console.log("Client: I've got a simple component");
+compositeClientCode(simple);
+console.log("");
+
+/**
+ * ...as well as the complex composites.
+ */
+const tree = new Composite();
+const branch1 = new Composite();
+branch1.add(new Leaf());
+branch1.add(new Leaf());
+const branch2 = new Composite();
+branch2.add(new Leaf());
+tree.add(branch1);
+tree.add(branch2);
+console.log("Client: Now I've got a composite tree");
+compositeClientCode(tree);
+console.log("");
+
+/**
+ * Thanks to the fact that the child-management operations are declared in the
+ * base Component class, the client code can work with any component, simple or
+ * complex, without depending on their concrete classes.
+ */
+const compositeClientCode2 = (component1: Component, component2: Component) => {
+  if (component1.isComposite()) {
+    component1.add(component2);
+  }
+  console.log(`Result: ${component1.operation()}`);
+};
+
+console.log(
+  "Client: I don't need to check the components classes even when managing the tree:"
+);
+compositeClientCode2(tree, simple);
